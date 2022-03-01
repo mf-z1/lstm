67ba1d44f666a51e699518358b5bbb59b968a3fc
mafan <2384503133@qq.com>
2022-03-01 17:13:22 +0800

    更新读取数据

--- refactor/datasets.py
+++ refactor/datasets.py
@@ -16,14 +16,12 @@ cache = '.vector_cache'  # 词向量的缓存位置
 
 def word_cut(text):
     text = regex.sub(' ', text)
-    # print(text)
     return [word for word in jieba.cut(text) if word.strip()]
 
 
 text_field = data.Field(lower=True)
 label_field = data.Field(sequential=False, eos_token=None, pad_token=None, unk_token=None)
 text_field.tokenize = word_cut  # 分词方法
-# print(text_field.tokenize)
 train, dev = data.TabularDataset.splits(
     path='.././data', format='tsv', skip_header=True,
     train='train.tsv', validation='dev.tsv',
@@ -33,8 +31,6 @@ train, dev = data.TabularDataset.splits(
         ('text', text_field)
     ]
 )
-# print(vars(dev.examples[0]))
-
 
 vectors = Vectors(name=embedding_loc, cache=cache)  # 构建词向量表
 
@@ -48,6 +44,6 @@ dev_iter = BucketIterator(dev, batch_size=64,
                           sort_key=lambda x: len(x.text), sort_within_batch=True, shuffle=True)
 
 vocab_size = text_field.vocab.vectors.shape  # 词表维度
-# print(vocab_size)
+
 if __name__ == '__main__':
     print(word_cut('我的时间2.0t不多了'))
